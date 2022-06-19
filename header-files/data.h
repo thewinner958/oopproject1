68887dd17c4ffdefea7a6889b7c2fbdaa491fef0
TheWinner <thewinner958@gmail.com>
2022-06-19 21:08:44 +0300

    created a data constructor

--- header-files/data.h
+++ header-files/data.h
@@ -7,6 +7,8 @@
 #include "table.h"
 #include "string.h"
 
+#define BUFFER 1000000000
+
 using namespace std;
 
 
@@ -16,20 +18,48 @@ using namespace std;
 class Data : public Table {
 private:
     vector<char *> mData;
+    int mNumberOfData = 0;
+    int mColumns = 0;
+    int mLines = 0;
 public:
     Data();
     Data(const Table& table);
-    ~Data();
+    ~Data() = default;
 };
 
 inline Data::Data() : Table() {
-    mData.end();
+    mData[0] = nullptr;
 }
 
 Data::Data(const Table& table) : Table(table){
-    char * temp = getTable();
-    for (int i = 0; i < sizeof(temp); ++i) {
-
+    char * tempTable = getTable();
+    char * temp = nullptr;
+    int tempColumns = 0;
+    for (int i = 0; i < sizeof(tempTable); ++i) {
+        if (tempTable[i] == ',') {
+            for (int j = 0; j < BUFFER; ++j) {
+                if (mData[j] != nullptr) {
+                    continue;
+                } else {
+                    mData[j] = temp;
+                    mNumberOfData++;
+                    temp = nullptr;
+                    break;
+                }
+            }
+            tempColumns++;
+        }
+        if (tempTable[i] == '\n') {
+            mLines++;
+            if (mColumns == 0) mColumns = tempColumns;
+            else if (mColumns != 0 && tempColumns > mColumns) {
+                mColumns = tempColumns;
+            } else mColumns = mColumns;
+            tempColumns = 0;
+            temp = nullptr;
+            continue;
+        }
+        temp += tempTable[i];
     }
 }
 
